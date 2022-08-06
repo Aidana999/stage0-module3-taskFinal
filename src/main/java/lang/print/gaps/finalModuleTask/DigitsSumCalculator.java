2e6237949357671d3a316489457227fb41079097
Aidana <49874037+Aidana999@users.noreply.github.com>
2022-08-06 23:43:46 +0600

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -3,21 +3,21 @@ package lang.print.gaps.finalModuleTask;
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
         int sum = 0;
-         
+
         if (number != 0)
         {
             sum = sum + number % 10;
             number = number/10;
         }
-     
-    return sum;
+
+        return sum;
     }
- 
+
     // Driver program
-    public static void main(String[] args)
+    public void main(String[] args)
     {
         int number = 687;
- 
-        System.out.println(getSum(number));
+
+        System.out.println(calculateSum(number));
     }
 }
