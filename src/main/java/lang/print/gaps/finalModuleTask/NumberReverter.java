c50868b84136506a1ed938608188eb67e8741b4b
Aidana <49874037+Aidana999@users.noreply.github.com>
2022-08-06 23:29:03 +0600

    Update NumberReverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,19 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int rev_num = 0;
+        while (number > 0) {
+            rev_num = rev_num * 10 + number % 10;
+            number = number / 10;
+        }
+        return rev_num;
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+        int num = 4562;
+        System.out.println("Reverse of no. is "
+                + revert(num));
     }
 }
