85604a8ea470387564908530a998cedd2132628e
Aidana <49874037+Aidana999@users.noreply.github.com>
2022-08-06 22:15:17 +0600

    Update TemperatureConverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -3,7 +3,6 @@ package lang.print.gaps.finalModuleTask;
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
         int Fahrenheit;
-        temperatureCelsius= 5;
         Fahrenheit =((temperatureCelsius*9)/5)+32;
         System.out.println(Fahrenheit);
     }
